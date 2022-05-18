ac2e341f384d17402ed262f93d7a3af9e7f174f5
yzh0120 <644193385@qq.com>
2022-05-18 14:36:25 +0800

    up

--- src/router/modules/5-file.js
+++ src/router/modules/5-file.js
@@ -1,20 +1,28 @@
+/*
+ * @Author: yz
+ * @Date: 2022-05-05 16:13:01
+ * @Description: 
+ * @FilePath: \workTo30_vue2Admin\src\router\modules\5-file.js
+ * 
+ */
 let layout = () => import("@/layout/index.vue")
-let blank = () => import("@/views/blank.vue")//二级菜单专属
+let blank = () => import("@/views/blank.vue") //二级菜单专属
 
-export default { 
+export default {
     path: '/file',
     name: 'file',
-    redirect: {name:"one-file"},
-    component:  layout,
+    redirect: {
+        name: "one-file"
+    },
+    component: layout,
     meta: {
-        title: '封装伸缩框',
+        title: '封装文件上传',
         icon: "wenjianshangchuan",
     },
-    children: [
-        {
+    children: [{
             path: 'one',
             name: 'one-file',
-            component: () => import(/* webpackChunkName: "file-base" */ "@/views/5-file/1-one-file.vue"),
+            component: () => import( /* webpackChunkName: "file-base" */ "@/views/5-file/1-one-file.vue"),
             meta: {
                 title: '单个文件',
                 icon: "wenjianshangchuan",
@@ -23,7 +31,7 @@ export default {
         {
             path: 'fileList',
             name: 'file-List',
-            component: () => import(/* webpackChunkName: "file-base" */ "@/views/5-file/2-file-list.vue"),
+            component: () => import( /* webpackChunkName: "file-base" */ "@/views/5-file/2-file-list.vue"),
             meta: {
                 title: '多个文件',
                 icon: "wenjianshangchuan",
--- src/views/5-file/2-file-list.vue
+++ src/views/5-file/2-file-list.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Author: yz
+ * @Date: 2022-05-05 16:13:01
+ * @Description: 
+ * @FilePath: \workTo30_vue2Admin\src\views\5-file\2-file-list.vue
+ * 
+-->
 <template>
   <div>
     <file-List :fileList="fileList" :del="true" />
@@ -13,6 +20,11 @@ export default {
           name: "xxx",
           taskName: "xxx",
           detail: [],
+          //type:[],
+
+          //require:
+
+          //num:
         },
       ],
     };
